1c2be95a6166297cd7b11febc859d3079fa6b81b
clodoveo <filippo@ramointeractive.com>
2020-10-26 17:42:49 +0100

    via bordi

--- src/components/ContenitoreUtenti.js
+++ src/components/ContenitoreUtenti.js
@@ -5,7 +5,7 @@ import ColonnaUtente from "./ColonnaUtente";
 const StyledContenitoreUtenti = styled.div`
   width: 563px;
   height: 340px;
-  border: 1px solid red;
+  /* border: 1px solid red; */
   position: absolute;
   left: 275px;
   top: 269px;
