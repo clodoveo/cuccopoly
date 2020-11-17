f7803c18617a70e3a650a521062abbedf4193226
clodoveo <filippo@ramointeractive.com>
2020-11-17 19:37:08 +0100

    form selezione motivazione utilizzo jolly

--- src/components/UsaJolly.js
+++ src/components/UsaJolly.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import UsaJollyForm from "./UsaJollyForm";
 export default function UsaJolly(props) {
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    setClicked((old) => !old);
+  }
   return (
-    <motion.button
-      animate={{ scale: 1.01, boxShadow: "3px 3px 5px 3px #999" }}
-      transition={{
-        repeat: Infinity,
-        repeatType: "reverse",
-        duration: 0.6
-      }}
-      style={{
-        position: "fixed",
-        top: "150px",
-        left: "115px",
-        height: "50px",
-        width: "150px",
-        backgroundColor: "#88123A",
-        border: "2px solid #fff",
-        color: "#fff",
-        fontFamily: "Times",
-        fontSize: "20px",
-        boxShadow: "1px 1px 2px 1px #999",
-        cursor: "pointer"
-      }}
-    >
-      USA JOLLY
-    </motion.button>
+    <>
+      <button
+        animate={{ scale: 1.01, boxShadow: "3px 3px 5px 3px #999" }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "reverse",
+          duration: 0.6
+        }}
+        style={{
+          position: "fixed",
+          top: "150px",
+          left: "115px",
+          height: "50px",
+          width: "150px",
+          backgroundColor: "#88123A",
+          border: "2px solid #fff",
+          color: "#fff",
+          fontFamily: "Times",
+          fontSize: "20px",
+          boxShadow: "1px 1px 2px 1px #999",
+          cursor: "pointer"
+        }}
+        onClick={handleClick}
+      >
+        USA JOLLY
+      </button>
+      {clicked && <UsaJollyForm toggle={handleClick} />}
+    </>
   );
 }
---
+++ src/components/UsaJollyForm.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default function UsaJollyForm(props) {
+  const disabledAvanza = true;
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div
+      style={{
+        backgroundColor: "#fff",
+        position: "fixed",
+        width: "467px",
+        height: "318px",
+        display: "flex",
+        alignItems: "center",
+        justifyItems: "center",
+        placeContent: "center",
+        padding: "20px 50px",
+        top: "250px",
+        left: "284px"
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          backgroundColor: "rgb(255, 255, 255)",
+          position: "fixed",
+          display: "flex",
+          placeItems: "center",
+          padding: "20px 50px",
+          justifyContent: "center",
+          flexDirection: "column"
+        }}
+      >
+        <button onClick={props.toggle}>X</button>
+        <div>
+          <label>
+            <h1 style={{ fontFamily: "Titillium Web" }}>Usa Jolly per:</h1>
+          </label>
+        </div>
+        <br />
+        <div style={{ marginBottom: "50px" }}>
+          <select>
+            <option selected>Seleziona...</option>
+            <option value="avanzare" disabled={disabledAvanza}>
+              Avanzare
+            </option>
+            <option value="ribasso prezzo non autorizzato">
+              ribasso prezzo non autorizzato
+            </option>
+          </select>
+        </div>
+        <div>
+          <button
+            type="submit"
+            style={{
+              height: "50px",
+              width: "150px",
+              backgroundColor: "#88123A",
+              border: "2px solid #fff",
+              color: "#fff",
+              fontFamily: "Times",
+              fontSize: "20px",
+              boxShadow: "1px 1px 2px 1px #999",
+              cursor: "pointer"
+            }}
+          >
+            USA JOLLY
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
